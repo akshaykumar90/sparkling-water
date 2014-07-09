b646ee4bd7af7dffa991cad9132dfa67cc3a70d5
Akshay Kumar <akshaykumar90@gmail.com>
2014-07-10 00:39:59 +0530

    Add parity.go

---
+++ chapter5/parity.go
@@ -0,0 +1,30 @@
+package chapter5
+
+var precomputedParity = buildTable()
+
+func buildTable() []uint8 {
+	table := make([]uint8, 1 << 16)
+
+	for i := 0; i < (1 << 16); i++ {
+		table[i] = uint8(parity(i))
+	}
+
+	return table
+}
+
+func parity(n int) int {
+	var p int
+	
+	for ; n > 0; n &= n-1 {
+		p ^= 1
+	}
+
+	return p
+}
+
+func Parity(n uint64) uint8 {
+	return precomputedParity[n & (1<<16 - 1)] ^ 
+		precomputedParity[(n >> 16) & (1<<16 - 1)] ^
+		precomputedParity[(n >> 32) & (1<<16 - 1)] ^
+		precomputedParity[(n >> 48) & (1<<16 - 1)]
+}
---
+++ chapter5/parity_test.go
@@ -0,0 +1,24 @@
+package chapter5
+
+import "testing"
+
+var parityTests = []struct {
+	n        uint64 // input
+	expected uint8 // expected result
+}{
+	{0, 0},
+	{1, 1},
+	{2, 1},
+	{3, 0},
+	{4, 1},
+	{1<<64 - 1, 0},
+}
+
+func TestParity(t *testing.T) {
+	for _, tt := range parityTests {
+		actual := Parity(tt.n)
+		if actual != tt.expected {
+			t.Errorf("Parity(%d): expected %d, actual %d", tt.n, tt.expected, actual)
+		}
+	}
+}
