671c696749149fcadcf0b1720228e3f5f8fb8b4e
Akshay Kumar <akshaykumar90@gmail.com>
2014-07-13 15:46:54 +0530

    Add interconvertingstringinteger.go

---
+++ chapter5/interconvertingstringinteger.go
@@ -0,0 +1,63 @@
+package chapter5
+
+import "errors"
+
+func StringToInt(s string) (int, error) {
+	if len(s) == 0 {
+		return 0, errors.New("StringToInt: invalid string")
+	}
+
+	mult, num := 1, 0
+
+	for i, ch := range s {
+		switch {
+		case ch == '-':
+			if i == 0 {
+				mult = -1
+			} else {
+				return 0, errors.New("StringToInt: invalid string")
+			}
+		case ch > '9' || ch < '0':
+			return 0, errors.New("StringToInt: invalid string")
+		default:
+			num = num*10 + int(ch-'0')
+		}
+	}
+
+	if mult == -1 && len(s) == 1 {
+		return 0, errors.New("StringToInt: invalid string")
+	}
+
+	return mult * num, nil
+}
+
+func IntToString(x int) string {
+	if x == 0 {
+		return "0"
+	}
+
+	slice := make([]rune, 0)
+
+	isNegative := false
+
+	if x < 0 {
+		x = -x
+		isNegative = true
+	}
+
+	for x > 0 {
+		slice = append(slice, '0'+rune(x%10))
+		x = x / 10
+	}
+
+	if isNegative {
+		slice = append(slice, '-')
+	}
+
+	n := len(slice)
+	for i := 0; i < n/2; i++ {
+		slice[i], slice[n-i-1] = slice[n-i-1], slice[i]
+	}
+
+	return string(slice)
+}
---
+++ chapter5/interconvertingstringinteger_test.go
@@ -0,0 +1,61 @@
+package chapter5
+
+import "testing"
+
+var validStringToIntTests = []struct {
+	s        string
+	expected int
+}{
+	{"0", 0},
+	{"1", 1},
+	{"123", 123},
+	{"-8", -8},
+	{"03213", 3213},
+	{"-00033", -33},
+}
+
+var invalidStringToIntTests = []string{
+	"", "-", "--123", "123-4", "456ab12", "wqff",
+}
+
+func TestStringToIntValid(t *testing.T) {
+	for _, tt := range validStringToIntTests {
+		actual, _ := StringToInt(tt.s)
+		if actual != tt.expected {
+			t.Errorf("StringToInt(%s): expected %d, actual %d",
+				tt.s, tt.expected, actual)
+		}
+	}
+}
+
+func TestStringToIntInvalid(t *testing.T) {
+	for _, tt := range invalidStringToIntTests {
+		actual, err := StringToInt(tt)
+		if err == nil {
+			t.Errorf("StringToInt(%s): expected error, actual %d",
+				tt, actual)
+		}
+	}
+}
+
+var intToStringTests = []struct {
+	x        int
+	expected string
+}{
+	{0, "0"},
+	{1, "1"},
+	{123, "123"},
+	{8143, "8143"},
+	{-8, "-8"},
+	{-3213, "-3213"},
+}
+
+func TestIntToString(t *testing.T) {
+	for _, tt := range intToStringTests {
+		actual := IntToString(tt.x)
+		if actual != tt.expected {
+			t.Errorf("IntToString(%d): expected %s, actual %s",
+				tt.x, tt.expected, actual)
+		}
+	}
+}
