91813e1451402cc6fbaa1257517a603979f8da0a
Akshay Kumar <akshaykumar90@gmail.com>
2014-07-17 22:24:21 +0530

    Add towerhanoi.go

---
+++ chapter8/towerhanoi.go
@@ -0,0 +1,16 @@
+package chapter8
+
+import "fmt"
+
+func _hanoi(n int, from, to, aux string) {
+	if n > 0 {
+		_hanoi(n-1, from, aux, to)
+		fmt.Printf("Ring %d : %s => %s", n, from, to)
+		fmt.Println()
+		_hanoi(n-1, aux, to, from)
+	}
+}
+
+func TowerHanoi(n int) {
+	_hanoi(n, "p1", "p2", "p3")
+}
---
+++ chapter8/towerhanoi_test.go
@@ -0,0 +1,11 @@
+package chapter8
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTowerHanoi(t *testing.T) {
+	TowerHanoi(3)
+	fmt.Println()
+}
