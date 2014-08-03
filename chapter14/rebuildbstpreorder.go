52206d1e7d61c86b71205be24dc074e331058299
Akshay Kumar <akshaykumar90@gmail.com>
2014-08-03 12:02:36 +0530

    Add RebuildBSTPreorderBetter

--- chapter14/rebuildbstpreorder.go
+++ chapter14/rebuildbstpreorder.go
@@ -2,6 +2,8 @@
 
 package chapter14
 
+import "math"
+
 func RebuildBSTPreorder(pre []int) *TreeNode {
 	if len(pre) == 0 {
 		return nil
@@ -17,4 +19,27 @@ func RebuildBSTPreorder(pre []int) *TreeNode {
 
 		return n
 	}
+}
+
+func rebuildBSTPreorderWithinLimits(pre []int, i, min, max int) (int, *TreeNode) {
+	if i == len(pre) {
+		return 0, nil
+	}
+
+	if pre[i] < min || pre[i] > max {
+		return 0, nil
+	} else {
+		n := new(TreeNode)
+		n.Value = pre[i]
+		j, left := rebuildBSTPreorderWithinLimits(pre, i+1, min, pre[i])
+		k, right := rebuildBSTPreorderWithinLimits(pre, i+j+1, pre[i], max)
+		n.Left = left
+		n.Right = right
+		return j + k + 1, n
+	}
+}
+
+func RebuildBSTPreorderBetter(pre []int) *TreeNode {
+	_, t := rebuildBSTPreorderWithinLimits(pre, 0, math.MinInt32, math.MaxInt32)
+	return t
 }
--- chapter14/rebuildbstpreorder_test.go
+++ chapter14/rebuildbstpreorder_test.go
@@ -5,20 +5,28 @@ import (
 	"testing"
 )
 
-func TestRebuildBSTPreorder(t *testing.T) {
-	var inOrderTraversal func(root *TreeNode)
-	inOrderTraversal = func(root *TreeNode) {
-		if root == nil {
-			return
-		} else {
-			inOrderTraversal(root.Left)
-			fmt.Println(root.Value)
-			inOrderTraversal(root.Right)
-		}
+func inOrderTraversal(root *TreeNode) {
+	if root == nil {
+		return
+	} else {
+		inOrderTraversal(root.Left)
+		fmt.Println(root.Value)
+		inOrderTraversal(root.Right)
 	}
+}
 
+func TestRebuildBSTPreorder(t *testing.T) {
 	root := RebuildBSTPreorder([]int{19, 7, 3, 2, 5, 11, 17, 13, 43, 23, 37, 29, 31, 41, 47})
 
+	fmt.Println("TestRebuildBSTPreorder:")
+	inOrderTraversal(root)
+	fmt.Println()
+}
+
+func TestRebuildBSTPreorderBetter(t *testing.T) {
+	root := RebuildBSTPreorderBetter([]int{19, 7, 3, 2, 5, 11, 17, 13, 43, 23, 37, 29, 31, 41, 47})
+
+	fmt.Println("TestRebuildBSTPreorderBetter:")
 	inOrderTraversal(root)
 	fmt.Println()
 }
