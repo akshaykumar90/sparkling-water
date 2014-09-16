6a537de956573dc158a2d8fad70a9815aa7d92ff
Akshay Kumar <akshaykumar90@gmail.com>
2014-09-16 22:29:20 +0530

    Add 17.8 - sudokusolve.go

--- chapter17/sudokusolve.go
+++ chapter17/sudokusolve.go
@@ -2,10 +2,29 @@
 
 package chapter17
 
-type tuple struct {
-	r, c, d int
+import "github.com/akshaykumar90/sparkling-water/chapter6"
+
+func sudokuSolveHelper(arr [][]int, i, j int) bool {
+	if j == len(arr[0]) {
+		i, j = i+1, 0
+	}
+	if i == len(arr) {
+		return true
+	}
+	if arr[i][j] > 0 {
+		return sudokuSolveHelper(arr, i, j+1)
+	} else {
+		for k := 0; k < 9; k++ {
+			arr[i][j] = k + 1
+			if chapter6.SudokuChecker(arr) && sudokuSolveHelper(arr, i, j+1) {
+				return true
+			}
+		}
+		arr[i][j] = 0
+		return false
+	}
 }
 
-func SudokuSolve(arr []tuple) {
-
+func SudokuSolve(arr [][]int) bool {
+	return chapter6.SudokuChecker(arr) && sudokuSolveHelper(arr, 0, 0)
 }
--- chapter17/sudokusolve_test.go
+++ chapter17/sudokusolve_test.go
@@ -4,40 +4,25 @@ import "testing"
 import "fmt"
 
 func TestSudokuSolve(t *testing.T) {
-	testSudoku := []tuple{
-		{0, 0, 5},
-		{0, 1, 3},
-		{0, 4, 7},
-		{1, 0, 6},
-		{1, 4, 1},
-		{1, 5, 9},
-		{1, 6, 5},
-		{2, 1, 9},
-		{2, 2, 8},
-		{2, 7, 6},
-		{3, 0, 8},
-		{3, 4, 6},
-		{3, 8, 3},
-		{4, 0, 4},
-		{4, 3, 8},
-		{4, 5, 3},
-		{4, 8, 1},
-		{5, 0, 7},
-		{5, 4, 2},
-		{5, 8, 6},
-		{6, 1, 6},
-		{6, 6, 2},
-		{6, 7, 8},
-		{7, 3, 4},
-		{7, 4, 1},
-		{7, 5, 9},
-		{7, 8, 5},
-		{8, 4, 8},
-		{8, 7, 7},
-		{8, 8, 9},
+	testSudoku := [][]int{
+		[]int{5, 3, 0, 0, 7, 0, 0, 0, 0},
+		[]int{6, 0, 0, 1, 9, 5, 0, 0, 0},
+		[]int{0, 9, 8, 0, 0, 0, 0, 6, 0},
+
+		[]int{8, 0, 0, 0, 6, 0, 0, 0, 3},
+		[]int{4, 0, 0, 8, 0, 3, 0, 0, 1},
+		[]int{7, 0, 0, 0, 2, 0, 0, 0, 6},
+
+		[]int{0, 6, 0, 0, 0, 0, 2, 8, 0},
+		[]int{0, 0, 0, 4, 1, 9, 0, 0, 5},
+		[]int{0, 0, 0, 0, 8, 0, 0, 7, 9},
 	}
 
-	fmt.Println("TestSudokuSolve:")
-	SudokuSolve(testSudoku)
-	fmt.Println()
+	if !SudokuSolve(testSudoku) {
+		t.Errorf("SudokuSolve: expected true, actual false")
+	} else {
+		for i := range testSudoku {
+			fmt.Println(testSudoku[i])
+		}
+	}
 }
