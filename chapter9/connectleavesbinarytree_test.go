3eb49e2c9213f38ad9b70b6245aa7f04116a0e84
Akshay Kumar <akshaykumar90@gmail.com>
2014-11-01 17:10:01 +0530

    Refactor Figure 9.1 - Example of a binary tree

--- chapter9/connectleavesbinarytree_test.go
+++ chapter9/connectleavesbinarytree_test.go
@@ -2,51 +2,53 @@ package chapter9
 
 import "testing"
 
-func TestConnectLeavesBinaryTree(t *testing.T) {
-	tree := &TreeElement{314,
-		&TreeElement{6,
-			&TreeElement{271,
-				&TreeElement{28, nil, nil},
-				&TreeElement{0, nil, nil},
-			},
-			&TreeElement{561,
+// Figure 9.1: Example of a binary tree
+var ExampleBinaryTree = &TreeElement{314,
+	&TreeElement{6,
+		&TreeElement{271,
+			&TreeElement{28, nil, nil},
+			&TreeElement{0, nil, nil},
+		},
+		&TreeElement{561,
+			nil,
+			&TreeElement{3,
+				&TreeElement{17, nil, nil},
 				nil,
-				&TreeElement{3,
-					&TreeElement{17, nil, nil},
+			},
+		},
+	},
+	&TreeElement{6,
+		&TreeElement{2,
+			nil,
+			&TreeElement{1,
+				&TreeElement{401,
 					nil,
+					&TreeElement{641, nil, nil},
 				},
+				&TreeElement{257, nil, nil},
 			},
 		},
-		&TreeElement{6,
-			&TreeElement{2,
-				nil,
-				&TreeElement{1,
-					&TreeElement{401,
-						nil,
-						&TreeElement{641, nil, nil},
-					},
-					&TreeElement{257, nil, nil},
-				},
-			},
-			&TreeElement{271,
-				nil,
-				&TreeElement{28, nil, nil},
-			},
+		&TreeElement{271,
+			nil,
+			&TreeElement{28, nil, nil},
 		},
-	}
+	},
+}
+
+func TestConnectLeavesBinaryTree(t *testing.T) {
 
 	expected := []int{28, 0, 17, 641, 257, 28}
 
-	actual := ConnectLeavesBinaryTree(tree)
+	actual := ConnectLeavesBinaryTree(ExampleBinaryTree)
 
 	if len(expected) != len(actual) {
 		t.Fatalf("ConnectLeavesBinaryTree(%v): expected length %d, actual length %d",
-			tree, len(expected), len(actual))
+			ExampleBinaryTree, len(expected), len(actual))
 	} else {
 		for i, v := range expected {
 			if v != actual[i].Value {
 				t.Fatalf("ConnectLeavesBinaryTree(%v): at %d: expected %d, actual %d",
-					tree, i, v, actual[i].Value)
+					ExampleBinaryTree, i, v, actual[i].Value)
 			}
 		}
 	}
