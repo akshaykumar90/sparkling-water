f3f41848db6496e613919afb201bb8bf5345cd69
Akshay Kumar <akshaykumar90@gmail.com>
2014-12-17 19:55:47 +0530

    Restructure 11.2 - binarysearchlargerk.go

--- chapter11/binarysearchlargerk.go
+++ chapter11/binarysearchlargerk.go
@@ -1,25 +1,19 @@
-// problem 11.2
+// Problem 11.2
 
 package chapter11
 
 func BinarySearchLargerK(arr []int, k int) int {
-	// arr[:lo] has all x <= k
-	// arr[hi:] has all x > k
-	lo, hi := 0, len(arr)
+	lo, hi, res := 0, len(arr)-1, -1
 
-	for lo < hi {
-		mid := (lo + hi - 1) / 2
+	for lo <= hi {
+		mid := lo + (hi-lo)/2
 		if arr[mid] <= k {
 			lo = mid + 1
 		} else {
-			hi = mid
+			res = mid
+			hi = mid - 1
 		}
 	}
 
-	if hi < len(arr) {
-		return hi
-	} else {
-		return -1
-	}
-
+	return res
 }
