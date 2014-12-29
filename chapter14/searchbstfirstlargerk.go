cb6135dd490566b00687cba35f03c73f4e7c5cf5
Akshay Kumar <akshaykumar90@gmail.com>
2014-12-29 12:40:08 +0530

    Refactor 14.4 - searchbstfirstlargerk.go

--- chapter14/searchbstfirstlargerk.go
+++ chapter14/searchbstfirstlargerk.go
@@ -1,36 +1,26 @@
-// problem 14.4
+// Problem 14.4
 
 package chapter14
 
-func search(root *TreeNode, k int, found_k bool) (*TreeNode, bool) {
-	if root == nil {
-		return nil, found_k
-	}
-
-	var succ *TreeNode
-
-	if !found_k {
-		if k == root.Value {
-			return search(root.Right, k, true)
-		} else if k < root.Value {
-			succ, found_k = search(root.Left, k, found_k)
-		} else {
-			return search(root.Right, k, found_k)
-		}
-	} else {
-		succ, found_k = search(root.Left, k, found_k)
-	}
-
-	if found_k && succ != nil {
-		return succ, found_k
-	} else if found_k {
-		return root, found_k
-	} else {
-		return nil, found_k
-	}
-}
-
 func SearchBSTFirstLargerK(root *TreeNode, k int) *TreeNode {
-	succ, _ := search(root, k, false)
-	return succ
+	var first *TreeNode
+	found := false
+
+	for root != nil {
+		if root.Value > k {
+			first = root
+			root = root.Left
+		} else {
+			if root.Value == k {
+				found = true
+			}
+			root = root.Right
+		}
+	}
+
+	if found {
+		return first
+	} else {
+		return nil
+	}
 }
