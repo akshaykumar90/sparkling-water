79a69a50e599c32c3bfcd3e321901d3fc1de37dd
Akshay Kumar <akshaykumar90@gmail.com>
2015-01-02 23:52:01 +0530

    Better tests for KThLargestElement

--- chapter10/kthlargestelement.go
+++ chapter10/kthlargestelement.go
@@ -1,24 +1,31 @@
-// problem 10.5
+// Problem 10.5
 
 package chapter10
 
 import (
 	"container/heap"
-	"fmt"
+	"github.com/akshaykumar90/sparkling-water/common"
 )
 
-func KThLargestElement(stream []int, k int) {
-	h := make(IntHeap, 0)
+func KThLargestElement(stream []int, k int) []int {
+	result := make([]int, 0, len(stream))
+
+	h := make(common.IntHeap, 0)
+
 	heap.Init(&h)
+
 	for i := 0; i < k && i < len(stream); i++ {
 		heap.Push(&h, stream[i])
-		fmt.Printf("kth largest after %d cycles: %d\n", i+1, h[0])
+		result = append(result, h[0])
 	}
+
 	for i := k; i < len(stream); i++ {
 		if stream[i] > h[0] {
 			heap.Pop(&h)
 			heap.Push(&h, stream[i])
 		}
-		fmt.Printf("kth largest after %d cycles: %d\n", i+1, h[0])
+		result = append(result, h[0])
 	}
+
+	return result
 }
--- chapter10/kthlargestelement_test.go
+++ chapter10/kthlargestelement_test.go
@@ -1,8 +1,9 @@
 package chapter10
 
 import (
-	"fmt"
+	"github.com/akshaykumar90/sparkling-water/common"
 	"math/rand"
+	"sort"
 	"testing"
 	"time"
 )
@@ -11,8 +12,24 @@ func TestKThLargestElement(t *testing.T) {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	stream := rand.Perm(100)
-	fmt.Println(stream)
+	k := 15
 
-	KThLargestElement(stream, 15)
-	fmt.Println()
+	expected := make([]int, 0, 100)
+
+	temp := make([]int, 100)
+	copy(temp, stream)
+	for i := range temp {
+		arr := temp[:i+1]
+		sort.Ints(arr)
+
+		if i+1 <= k {
+			expected = append(expected, arr[0])
+		} else {
+			expected = append(expected, arr[len(arr)-k])
+		}
+	}
+
+	actual := KThLargestElement(stream, k)
+
+	common.AssertIntsAreEqual(t, "KThLargestElement", expected, actual)
 }
